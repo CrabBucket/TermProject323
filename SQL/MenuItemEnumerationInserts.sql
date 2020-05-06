8b29f912894994c5bd34e542c094c926d44039c6
CrabBucket <tom@mcratracing.com>
2020-05-06 05:54:08 -0700

    Populate all of MenuItems

--- SQL/MenuItemEnumerationInserts.sql
+++ SQL/MenuItemEnumerationInserts.sql
@@ -1,3 +1,6 @@
 INSERT INTO SpiceLevel (SpiceLevel) VALUES ("Mild"), ("Tangy"), ("Piquant"), ("Hot"), ("Oh My God");
 
 INSERT INTO MeatType (MeatName) VALUES ("Chef Special"), ("Pork"), ("Chicken"), ("Beef"), ("Seafood"), ("Vegetables");
+
+
+INSERT INTO MenuType (MenuType) VALUES ("Evening"), ("Lunch"), ("Sunday Brunch Buffet"), ("Childrens");
--- SQL/MenuItemsPopulateTables.sql
+++ SQL/MenuItemsPopulateTables.sql
@@ -1,5 +1,6 @@
-START TRANSACTION;
-
+#SET autocommit = OFF;
+#START TRANSACTION;
+SAVEPOINT beforeinsert;
 INSERT INTO FoodItem (DisplayName,Price,SpiceLevel) VALUES ('Egg Drop',10.99,'Tangy'),('Spicy Udon',8.99,'Hot'),('Pork Ramen',10.99,'Mild'),('Chicken Noodle',4.99,'Mild'),('Wonton Soup',6.99,'Mild');
 INSERT INTO Soup (FoodItemID) 
 (SELECT FoodItemID FROM FoodItem);
@@ -9,7 +10,7 @@ INSERT INTO Appetizer (FoodItemID)
 SELECT FoodItemID FROM FoodItem WHERE FoodItem.FoodItemID NOT IN (Select FoodItemID FROM Soup);
 
 INSERT INTO FoodItem (DisplayName,Price,SpiceLevel) VALUES ('Orange Chicken',12.99,'Tangy'),('Porkchop',15.99,'Hot'),('Steak',18.99,'Mild'),('Kung Pao Chicken',14.99,'Oh My God'),('Broccoli Beef',12.99,'Mild');
-CREATE VIEW soupapp AS  SELECT FoodItemID FROM (SELECT * FROM Soup ) tmp UNION (Select FoodItemID FROM Appetizer tmp2);
+#CREATE VIEW soupapp AS  SELECT FoodItemID FROM (SELECT * FROM Soup ) tmp UNION (Select FoodItemID FROM Appetizer tmp2);
 INSERT INTO MeatEntree (FoodItemID,MeatName)
 SELECT FoodItemID,'Chicken' FROM FoodItem WHERE FoodItemID NOT IN (SELECT FoodItemID FROM soupapp);
 
@@ -24,5 +25,22 @@ INSERT INTO MenuListing (DisplayName,DiscountRate) VALUES ('Orange Chicken Dinne
 INSERT INTO MenuDeals (MenuListingID, FoodItemID) VALUES (16,11),(16,6),(16,1),(17,12),(17,7),(17,2);
 
 INSERT INTO MenuDeals (MenuListingID, FoodItemID)
-	(SELECT 18, FoodItemID FROM FoodItems);
-COMMIT;
+	(SELECT 18, FoodItemID FROM FoodItem);
+INSERT INTO Menu (MenuType) SELECT MenuType FROM MenuType;
+INSERT INTO MenuOfferings (MenuID,MenuListingID) 
+	SELECT 1,MenuListingID FROM MenuListing;
+INSERT INTO MenuOfferings (MenuID,MenuListingID) 
+	SELECT 2,MenuListingID FROM MenuListing;
+INSERT INTO MenuOfferings (MenuID,MenuListingID) 
+	SELECT 3,MenuListingID FROM MenuListing;
+#INSERT INTO MenuOfferings (MenuID,MenuListingID) 
+#	SELECT 3,MenuListingID FROM MenuListing;
+
+INSERT INTO MenuPrice (MenuType,MenuRate) VALUES ('Lunch',1),('Evening',1.2),('Childrens',0.8);
+INSERT INTO BuffetPrice (MenuType,Price) VALUEs ('Sunday Brunch Buffet',25.50);
+
+
+
+#COMMIT;
+
+#ROLLBACK TO beforeinser;
